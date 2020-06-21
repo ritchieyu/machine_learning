c0c285de09113beaead30f230e1020e0ced710f9
ritchieyu <67218704+ritchieyu@users.noreply.github.com>
2020-06-21 00:56:18 -0600

    Update computeCost.m

--- computeCost.m
+++ computeCost.m
@@ -1,5 +1,5 @@
 % Compute cost function for any m number of linear regression variables 
 function J = computeCost(X, y, theta) % X is design matrix 
 m = length(y); % number of training examples
-J = (1/(2*m))*transpose(X*theta - y)*(X*theta - y); % Vectorization
+J = (1/(2*m))*transpose(X*theta - y)*(X*theta - y); % vectorization
 end
